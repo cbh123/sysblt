76e0964edc67aae2565995ef647b6aa7df4c794e
Charlie Holtz <charlieholtz@gmail.com>
2018-09-06 14:25:32 -0400

    v1 to show. no functionality

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log('hello')
+
+
+$ = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	$(id).style.display ='block';
+}
+var hide = function(id) {
+	$(id).style.display ='none';
+}
+
